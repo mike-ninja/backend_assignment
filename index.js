71fa6d548d2ad9451d747171849b0f22258153ac
mike_ninja <mikeedubas@hotmail.com>
2023-02-17 21:21:30 +0200

    update

--- index.js
+++ index.js
@@ -37,26 +37,23 @@ class Move
 {
     constructor()
     {
-        let row,col;
+        let row, col;
     }
 }
- 
  
 // This function returns true if there are moves
 // remaining on the board. It returns false if
 // there are no moves left to play.
-function isMovesLeft(board)
-{
-    for(let i = 0; i < 3; i++)
-        for(let j = 0; j < 3; j++)
-            if (board[i][j] == '-')
-                return true;
+//function isMovesLeft(board)
+//{
+    //for(let i = 0; i < 3; i++)
+        //for(let j = 0; j < 3; j++)
+            //if (board[i][j] == '-')
+                //return true;
                  
-    return false;
-}
+    //return false;
+//}
  
-// This is the evaluation function as discussed
-// in the previous article ( http://goo.gl/sJgv68 )
 function evaluate(b, game)
 {
      
@@ -67,10 +64,10 @@ function evaluate(b, game)
             b[row][1] == b[row][2])
         {
             if (b[row][0] == game.champ)
-                return +10;
+                return +1;
                  
             else if (b[row][0] == game.enemy)
-                return -10;
+                return -1;
         }
     }
   
@@ -81,10 +78,10 @@ function evaluate(b, game)
             b[1][col] == b[2][col])
         {
             if (b[0][col] == game.champ)
-                return +10;
+                return +1;
   
             else if (b[0][col] == game.enemy)
-                return -10;
+                return -1;
         }
     }
   
@@ -92,149 +89,77 @@ function evaluate(b, game)
     if (b[0][0] == b[1][1] && b[1][1] == b[2][2])
     {
         if (b[0][0] == game.champ)
-            return +10;
+            return +1;
              
         else if (b[0][0] == game.enemy)
-            return -10;
+            return -1;
     }
   
     if (b[0][2] == b[1][1] &&
         b[1][1] == b[2][0])
     {
         if (b[0][2] == game.champ)
-            return +10;
+            return +1;
              
         else if (b[0][2] == game.enemy)
-            return -10;
+            return -1;
     }
   
     // Else if none of them have
     // won then return 0
     return 0;
 }
-// This is the minimax function. It
-// considers all the possible ways
-// the game can go and returns the
-// value of the board
-function minimax(board, game, depth, isMax)
-{
-    let score = evaluate(board, game);
 
-    // If Maximizer has won the game
-    // return his/her evaluated score
-    if (score == 10)
-        return score;
-    
-    // If Minimizer has won the game
-    // return his/her evaluated score
-    if (score == -10)
-        return score;
-    
-    // If there are no more moves and
-    // no winner then it is a tie
-    if (isMovesLeft(board) == false)
-        return 0;
-    
-    // If this maximizer's move
-    if (isMax) // This doesnt seem to be used
+function check_enemy_move(board, game)
+{
+    let eval = 0;
+
+    for(let i = 0; i < 3; i++)
     {
-        console.log(`Checking moves for myself`)
-        let best = -1000;
-  
-        // Traverse all cells
-        for(let i = 0; i < 3; i++)
-        {
-            for(let j = 0; j < 3; j++)
+        for(let j = 0; j < 3; j++)
+        {     
+            if (board[i][j] == '-')
             {
-                 
-                // Check if cell is empty
-                if (board[i][j]=='-')
-                {
-                     
-                    // Make the move
-                    board[i][j] = game.champ;
-  
-                    // Call minimax recursively
-                    // and choose the maximum value
-                    best = Math.max(best, minimax(board,
-                                    depth + 1, !isMax));
-  
-                    // Undo the move
-                    board[i][j] = '-';
-                }
+                board[i][j] = game.enemy;
+                let best = evaluate(board, game);
+                board[i][j] = '-';
+                if (best == 1 || best == -1)
+                    return best;
             }
         }
-        return best;
     }
-  
-    // If this minimizer's move
-    else
-    {
-        let best = 1000;
-        // Traverse all cells
-        for(let i = 0; i < 3; i++)
-        {
-            for(let j = 0; j < 3; j++)
-            {
-                 
-                // Check if cell is empty
-                if (board[i][j] == '-')
-                {
-                     
-                    // Make the move
-                    board[i][j] = game.enemy;
-  
-                    // Call minimax recursively and
-                    // choose the minimum value
-                    best = Math.min(best, minimax(board,
-                                    depth + 1, !isMax));
-                    //console.log(`best enemy ? ${best}`);
-                    // Undo the move
-                    board[i][j] = '-';
-                }
-            }
-        }
-        return best;
-    }
+    return eval;
 }
  
-// This will return the best possible
-// move for the game.champ
 function findBestMove(board, game)
 { 
-    let bestVal = -1000;
+    let bestVal = 0;
     let bestMove = new Move();
     bestMove.row = -1;
     bestMove.col = -1;
-  
-    // Traverse all cells, evaluate
-    // minimax function for all empty
-    // cells. And return the cell
-    // with optimal value.
+
+    console.log(`${board}`);
     for(let i = 0; i < 3; i++)
     {
         for(let j = 0; j < 3; j++)
         {
-            // Check if cell is empty
             if (board[i][j] == '-')
             {
-                // Make the move
-                board[i][j] = game.champ;
-                // compute evaluation function
-                // for this move.
-                let moveVal = minimax(board, game, 0, false);
-                console.log(`row ${i} col ${j}: moveVal ${moveVal}`);
-                // Undo the move
-                board[i][j] = '-';
-  
-                // If the value of the current move
-                // is more than the best value, then
-                // update best
-                if (moveVal > bestVal)
+                let moveVal = check_enemy_move(board, game);
+                if (moveVal < bestVal)
                 {
+                    console.log(`PROTECTION row ${i} col ${j} moveVal ${moveVal}`);
                     bestMove.row = i;
                     bestMove.col = j;
-                    bestVal = moveVal;
+                }
+                board[i][j] = game.champ;
+                let ret = evaluate(board, game);
+                board[i][j] = '-';
+                if (ret == 1 || bestVal == 0)
+                {
+                    console.log(`FOR WIN row ${i} col ${j} moveVal ${ret}`);
+                    bestMove.row = i;
+                    bestMove.col = j;
                 }
             }
         }
@@ -317,9 +242,10 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/v1/games', (req, res) => {
-	if (!games.length)
-		res.end(`There are currently ${games.length} games`)
-	res.status(200).send(games);
+	res.status(200).send({
+        message: "Active Games",
+        games: games
+    });
 });
 
 app.post('/api/v1/games', (req, res) => {
@@ -330,17 +256,18 @@ app.post('/api/v1/games', (req, res) => {
 						[ '-', '-', '-' ],
 						[ '-', '-', '-' ] ];
 	
-	if (!board || !validate_board(board, board_param, new_game) || !new_game) 
-	{
-		res.status(400).send({ message: 'New game not created.'})
-	}
+	if (!board || !validate_board(board, board_param, new_game) || !new_game)
+    {
+		res.status(400).send({ message: 'New game not created.'});
+
+    }
 	else
 	{
 		games.push(new_game);
 		
 		let bestMove = findBestMove(board_param, new_game);
 		var index = (bestMove.row) * 3 + (bestMove.col);
-		console.log(`col ${bestMove.col} row ${bestMove.row} ${new_game.champ} pos ${new_game.board[(bestMove.col) * (bestMove.row)]}`);
+		//console.log(`col ${bestMove.col} row ${bestMove.row} ${new_game.champ} pos ${new_game.board[(bestMove.col) * (bestMove.row)]}`);
 
 		// convert the string to an array
 		let str = new_game.board;
@@ -354,13 +281,11 @@ app.post('/api/v1/games', (req, res) => {
 		str = arr.join("");
 		new_game.board = str;
 
-		console.log(`board ${new_game.board} val ${new_game.board[(bestMove.col + 1) * (bestMove.row + 1)]}`);
+		//console.log(`board ${new_game.board} val ${new_game.board[(bestMove.col + 1) * (bestMove.row + 1)]}`);
+		
 		res.status(200).send({
 		new_game: `Game at http://localhost:${PORT}/api/v1/games/${new_game.id}`,
-		incoming_board: `${board}`,
 		board: `Board status [${new_game.board}]`,
-        champ: `champ ${new_game.champ}`,
-        enemy: `enemy ${new_game.enemy}`,
 		});
 	}
 });
@@ -399,7 +324,7 @@ app.put('/api/v1/games/:id', (req, res) =>
 		let arr = str.split("");
 		
 		// update the character at index 2
-		console.log(`col ${bestMove.col} row ${bestMove.row} ${game.champ} pos ${game.board[(bestMove.col) * (bestMove.row)]}`);
+		//console.log(`col ${bestMove.col} row ${bestMove.row} ${game.champ} pos ${game.board[(bestMove.col) * (bestMove.row)]}`);
 		arr[index] = game.champ;
 
 		// convert the array back to a string
